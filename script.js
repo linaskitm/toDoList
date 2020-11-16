ed2864b4dd0b69febf77cdb700c92f7d8e2a9f10
Linas Pelenis <lpelenis@gmail.com>
2020-11-16 13:45:37 +0200

    added delete button, progress bar

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const status = document.getElementById('status');
 const percentage = document.getElementById('percentage');
 const submit = document.getElementById('submit');
 const list = document.getElementById('list');
+const alertMsg = document.getElementById('alert')
  
 document.getElementById('submit').addEventListener('click', function(e){
     e.preventDefault()
   });
 submit.addEventListener('click', createList);
+// resetinu inputo fieldus po submito
+submit.addEventListener('click', reset);
+function reset(){
+    // id reset uzdejau ant formos
+    document.getElementById('reset').reset(); 
+}
 
 function createList() {
     if((subject.value) && (priority.value)  && (status.value) && (percentage.value) ) {
@@ -21,9 +28,9 @@ function createList() {
     check.setAttribute('type', 'checkbox');
     row.appendChild(addCheck);
     addCheck.appendChild(check);
-    check.addEventListener("click", crossingOut);
+    check.addEventListener('click', crossingOut);
     function crossingOut(){
-        console.log("checked")
+        console.log(row);
         if(check.checked){
             row.classList.add('checked');
         } else {
@@ -37,6 +44,15 @@ function createList() {
 
      const b = document.createElement('td');
      b.innerHTML = priority.value;
+        if(priority.value == 'low') {
+            b.classList.add('text-success');
+            if(priority.value == 'normal') {
+                b.classList.add('text-primary');
+            }
+        } else {
+            b.classList.add('text-danger');
+        }
+            
      row.appendChild(b);
 
      const c = document.createElement('td');
@@ -48,11 +64,50 @@ function createList() {
      row.appendChild(d);
 
      const e = document.createElement('td');
-     e.innerHTML = percentage.value;
+     const value = percentage.value;
+     console.log(value);
+     switch (value) {
+         case '25%':
+        e.innerHTML = `<div class="progress">
+     <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+   </div>`; 
+        break;
+        case '50%':
+            e.innerHTML = `<div class="progress">
+     <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+   </div>`; 
+        break;
+        case '75%':
+            e.innerHTML = `<div class="progress">
+     <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+   </div>`; 
+        break;
+        case '100%':
+            e.innerHTML = `<div class="progress">
+     <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
+   </div>`; 
+        break;
+        default:
+            e.innerHTML = percentage.value;
+     }
      row.appendChild(e);
+
+     const deleteBtn = document.createElement('td');
+     const delete1 = document.createElement('input');
+     delete1.setAttribute('type', 'button');
+     delete1.setAttribute('value', 'Delete');
+     row.appendChild(deleteBtn);
+     deleteBtn.appendChild(delete1);
+
+     delete1.addEventListener('click', e => e.preventDefault());
+     delete1.addEventListener('click', function(){
+         console.log(list);
+         list.removeChild(list.childNodes[0]);
+     });
+
      
     } else  {
-        alert("Fill All Fields");
+        alert('Fill all Fields')
         return;
     }
 }
